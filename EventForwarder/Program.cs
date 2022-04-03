9f0677eea8a103b04ab5bd11605f7437e06426ee
Paul Mulder <Paul.Mulder@ogd.nl>
2022-04-03 23:46:19 +0200

    Add project files.

---
+++ EventForwarder/Program.cs
@@ -0,0 +1,10 @@
+using EventForwarder;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
